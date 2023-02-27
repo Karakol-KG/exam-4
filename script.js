dc99d233368970f32ff84b604f681797a86f1bcd
Karakol-KG <rustamabdyrahmanov10@gmail.com>
2023-02-27 19:32:05 +0600

    exam

---
+++ script.js
@@ -0,0 +1,13 @@
+const inp1=document.querySelector("#a")
+const btn=document.querySelector(".btn")
+const name=[Amantur,Meder,Misha,Baha]
+btn.addEventListener("click",(a-z)=>{
+
+    console.log(reduce)
+})
+
+const btn2=document.querySelector(".save")
+btn2=addEventListener("click",(event)=>{
+    event.preventDefault()
+    console.log(name)
+})
